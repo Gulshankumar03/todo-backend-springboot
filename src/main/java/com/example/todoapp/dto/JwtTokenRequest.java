b5b194b6084653b43e980c798db762a14d46d806
Gulshan Kumar <gulshangk1134@gmail.com>
2024-11-12 17:46:23 +0530

    Replaced basic auth with JWT

---
+++ src/main/java/com/example/todoapp/dto/JwtTokenRequest.java
@@ -0,0 +1,3 @@
+package com.example.todoapp.dto;
+
+public record JwtTokenRequest(String username, String password) {}
---
+++ src/main/java/com/example/todoapp/dto/JwtTokenResponse.java
@@ -0,0 +1,3 @@
+package com.example.todoapp.dto;
+
+public record JwtTokenResponse(String token) {}
